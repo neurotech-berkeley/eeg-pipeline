258e6445993dae9b7d65e0296ffbee9b80a51cff
Abhinav Pottabathula <apottabathula@berkeley.edu>
2020-09-13 01:16:22 -0700

    Reformatted EEG recording structure and added ability to start, stop, and download recordings

--- pages/visualizations.js
+++ pages/visualizations.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/layout'
-import { channelNames, EEGReading, MuseClient } from 'muse-js';
+import { channelNames, EEGReading, zipSamples, EEGSample, MuseClient } from 'muse-js';
+import { Observable, Subject } from 'rxjs';
 import io from 'socket.io-client';
 import * as Papa from 'papaparse';
 
@@ -10,40 +11,53 @@ const socket = io('http://localhost:5000');
 const museClient = new MuseClient();
 
 function Visualizations() {
-  const [testEEGData, setTestEEGData] = useState("");
   const [blink_state, setBlinkState] = useState("");
 
-  var fileList
+  var fileList;
+
+  var recording = false;
+  var recordingSamples;
+  var subscription;
 
   async function startMuseConnection(e) {
     await museClient.connect()
     await museClient.start()
-    museClient.eegReadings.subscribe(reading => {
-      socket.emit('eeg-stream', JSON.stringify(reading))
-      console.log(reading);
-    });
-    museClient.telemetryData.subscribe(telemetry => {
-      console.log(telemetry);
-    });
-    museClient.accelerometerData.subscribe(acceleration => {
-      console.log(acceleration);
+    // zipSamples(museClient.eegReadings).subscribe(sample => {
+    //   socket.emit('eeg-stream', [sample.timestamp, ...sample.data]);
+    // });
+    // museClient.telemetryData.subscribe(telemetry => {
+    //   console.log(telemetry);
+    // });
+    // museClient.accelerometerData.subscribe(acceleration => {
+    //   console.log(acceleration);
+    // });
+  }
+
+  function startRecording() {
+    recording = true;
+    recordingSamples = [];
+    subscription = zipSamples(museClient.eegReadings).subscribe(sample => {
+      sample = [sample.timestamp, ...sample.data]
+      recordingSamples.push(sample);
+      socket.emit('eeg-stream', sample);
     });
   }
 
-  async function streamCSVData(e) {
-    Papa.parse(fileList[0], {
-	    complete: function(results) {
-        var rows = results.data
-        var i = 0
-        var interval = setInterval(function() {
-          socket.emit('eeg-stream', rows[i]); 
-          i++; 
-          if(i >= rows.length) {
-            clearInterval(interval)
-          }
-        }, 3.90625)
-	    }
-    });
+  function stopRecording() {
+    recording = false;
+    subscription.unsubscribe();
+  }
+
+  function downloadRecording(e) {
+    const a = document.createElement('a');
+    const headers = ['time', ...channelNames].join(',');
+    const csvData = headers + '\n' + recordingSamples.map(item => item.join(',')).join('\n');
+    const file = new Blob([csvData], { type: 'text/csv' });
+    a.href = URL.createObjectURL(file);
+    document.body.appendChild(a);
+    a.download = 'recording.csv';
+    a.click();
+    document.body.removeChild(a);
   }
 
   useEffect(() => {
@@ -67,10 +81,20 @@ function Visualizations() {
   }, []);
 
 
-  function sendTestEEGData(evt) {
-    evt.preventDefault();
-    const data = '{"data":"' + testEEGData + '"}'
-    socket.emit('test', data);
+  async function streamCSVData(e) {
+    Papa.parse(fileList[0], {
+	    complete: function(results) {
+        var rows = results.data
+        var i = 0
+        var interval = setInterval(function() {
+          socket.emit('eeg-stream', rows[i]); 
+          i++; 
+          if(i >= rows.length) {
+            clearInterval(interval)
+          }
+        }, 3.90625)
+	    }
+    });
   }
 
   return (
@@ -86,21 +110,28 @@ function Visualizations() {
       <br></br>
       <br></br>
 
-      <input type="file" id="file-selector"></input>
-      <button onClick={streamCSVData}>
-        TEST!
+      <button onClick={startRecording}>
+        Start Recording
+      </button>
+
+      <button onClick={stopRecording}>
+        Stop Recording
+      </button>
+
+      <button onClick={downloadRecording}>
+        Download Recording
       </button>
 
       <br></br>
       <br></br>
 
-      <form onSubmit={sendTestEEGData}>
-        <label>
-        Send Test Message to Flask: <tab></tab>
-          <input type="text" name={testEEGData} onChange={e => setTestEEGData(e.target.value)}/>
-        </label>
-        <input type="submit" value="Send"/>
-      </form>
+      <input type="file" id="file-selector"></input>
+      <button onClick={streamCSVData}>
+        TEST!
+      </button>
+      
+      <br></br>
+
       <p>Blink State: {blink_state}</p>
     </Layout>
   );
